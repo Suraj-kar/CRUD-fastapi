f27bb8744a51756ed22e872be76b7f902359f5bb
suraj <karkisuraj665@gmail.com>
2025-12-14 12:35:47 +0545

    ci/cd added

--- tests/test_main.py
+++ tests/test_main.py
@@ -22,7 +22,7 @@ def test_read_item():
 def test_read_all_items():
     response = client.get("/items/")
     assert response.status_code == 200
-    # Assuming items are there from previous testss
+    # Assuming items are there from previous testsss
 
 def test_update_item():
     client.post("/items/3", json={"name": "Old", "description": "Old"})
